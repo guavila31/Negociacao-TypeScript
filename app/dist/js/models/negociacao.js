c7a0473868b939bfe54f92e73588f83ddcbc6939
guavila31 <gustavoavila2000@hotmail.com>
2022-10-10 17:45:11 -0300

    TypeScript parte 2: avançando na linguagem

--- app/dist/js/models/negociacao.js
+++ app/dist/js/models/negociacao.js
@@ -8,13 +8,14 @@ export class Negociacao {
         const data = new Date(this._data.getTime());
         return data;
     }
-    // get quantidade(): number {
-    //     return this._quantidade;
-    // }
-    // get valor(): number {
-    //     return this._valor;
-    // }
     get volume() {
         return this.quantidade * this.valor;
+    }
+    static criaDe(dataString, quantidadeString, valorString) {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
     }
 }
---
+++ app/src/controllers/negociacao-controller.ts
@@ -0,0 +1,60 @@
+import { logarTempoDeExecucao } from "../decorators/logarTempo-de-execucao.js";
+import { DiaDaSemana } from "../enums/dia-da-semana.js";
+import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
+import { MensagemView } from "../views/mensagem-view.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
+
+export class NegociacaoController {
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
+    private negociacoesView = new NegociacoesView("#negociacoesView", true);
+    private mensagemView = new MensagemView("#mensagemView");
+
+    constructor() {
+        this.inputData = document.querySelector("#data") as HTMLInputElement;
+        this.inputQuantidade = document.querySelector("#quantidade") as HTMLInputElement;
+        this.inputValor = document.querySelector("#valor") as HTMLInputElement;
+        this.negociacoesView.update(this.negociacoes);
+
+    }
+    
+   @logarTempoDeExecucao()
+    public adiciona(): void {
+        const negociacao = Negociacao.criaDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        );
+
+        if (!this.ehDiaUtil(negociacao.data)) {
+            this.mensagemView.update('Apenas negociaçoes em dias úteis são aceitas')
+            return;
+        }
+        this.negociacoes.adiciona(negociacao);
+        this.limparFormulario();
+        this.atualizaView();
+    }
+
+
+
+
+    private ehDiaUtil(data: Date) {
+        return data.getDay() > DiaDaSemana.DOMINGO && data.getDay() < DiaDaSemana.SABADO
+    }
+
+    public limparFormulario(): void {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+
+    }
+
+    private atualizaView() {
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso!')
+    }
+}
